62480c0fd15e1d5267938631e8aeaa7f0d8e0db6
r2dev2 <ronak.badhe@gmail.com>
2023-06-03 10:10:13 -0700

    auto-update chromedriver version to match chrome

--- autoparaselenium/browsers/chrome.py
+++ autoparaselenium/browsers/chrome.py
@@ -6,7 +6,9 @@ import sys
 from contextlib import suppress
 from functools import partial
 from pathlib import Path
+from typing import Optional
 
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -59,10 +61,45 @@ def get_selenium(pwd: Path, conf: Conf) -> webdriver.Chrome:
     return browser
 
 
-def setup_driver(pwd) -> None:
-    __setup_driver(pwd)
-    if (pwd / "chromedriver").exists():
+def setup_driver(pwd: Path) -> None:
+    chrome_version = __get_chrome_version()
+    driver_version = __get_chromedriver_version(pwd)
+    if driver_version is None or driver_version < chrome_version:
+        r = requests.get(
+            "https://chromedriver.storage.googleapis.com"
+            f"/LATEST_RELEASE_{chrome_version}"
+        )
+        version = r.text.strip()
+        os.remove(pwd / "chromedriver")
+        __setup_driver(version)(pwd)
         os.chmod(pwd / "chromedriver", stat.S_IEXEC)
+
+
+def __get_chromedriver_version(pwd: Path) -> Optional[int]:
+    with suppress(Exception):
+        # version returned will be the major version
+        p = sb.Popen([str(pwd / "chromedriver"), "--version"], stdout=sb.PIPE)
+        atexit.register(p.terminate)
+
+        # output of cmd will be lik
+        # ChromeDriver 108.0.5359.71 (1e0e3868ee06e91ad6...
+        version = p.communicate()[0].decode().split(" ")[1]
+        major, *_ = version.split(".")
+
+        # will raise ValueError if major version isn't a proper number
+        return int(major)
+
+
+def __get_chrome_version() -> int:
+    p = sb.Popen([__platform_binaries[su.platform], "--version"], stdout=sb.PIPE)
+    atexit.register(p.terminate)
+
+    # output of cmd will be lik
+    # Google Chrome 108.0.5359.124
+    major = p.communicate()[0].decode().split(".")[0].split(" ")[-1]
+
+    # will raise ValueError if major version isn't a proper number
+    return int(major)
 
 
 def __get_options(conf: Conf) -> Options:
@@ -84,9 +121,13 @@ __platform_drivers = {
     "linux": "chromedriver",
 }
 
-version = "108.0.5359.71"
+__platform_binaries = {
+    "darwin": "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    "win": os.path.expanduser(r"~\AppData\Local\Google\Chrome\Application\chrome.exe"),
+    "linux": "/usr/bin/google-chrome",
+}
 
-__setup_driver = partial(
+__setup_driver = lambda version: partial(
     su.setup_driver,
     {
         "win": [
